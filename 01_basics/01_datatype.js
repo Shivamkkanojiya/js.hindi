e62f9bbbc268b99c9d319f6d26dfcd68d07bf580
Shivam Kanojiya <shivamkanojiya161@gmail.com>
2025-04-20 09:41:09 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") we are using nodejs, not browser
+
+console.log(3+3) //code readability should be high
+console.log("Shivam")
+
+
+let name = "shivam"
+let age = 26
+let isLoggedIn = false
+
+// number = 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "shivam@gmail.com"
+var accountPassword = "12345"
+accountCity = "Varanasi"
+let accountState;
+
+//accountId = 2 //not allowed hai 
+
+accountEmial = "shiv@gmail.com"
+accountPassword = "1212121"
+accountCity = "mumbai"
+
+
+// {} ko scope bolte hai 
+
+/*
+Prefer not to use var 
+bacause of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
