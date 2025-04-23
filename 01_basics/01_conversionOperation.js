504d691af1dc63d258c4dd940f0269a3d4adfd31
Shivam Kanojiya <shivamkanojiya161@gmail.com>
2025-04-23 14:44:02 +0000

    operation learning

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,97 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+let score = "Shivam"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33 me conver ho jata hai 
+// "33abc" = NaN value deta hai yah 33 me convert nhi hota hai 
+// true => jo conver ho jata hai 1 and false ho jata hai 0 ke andar
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+//"" => false
+// "shivam" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+// Operation
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " shviam"
+
+let str3= str1+str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+let x= 3
+const y = x++
+ console.log(`x:${x}, y:${y}`);
+
+let a= 3
+const b = ++a
+
+console.log(`a:${a}, b:${b}`);
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
